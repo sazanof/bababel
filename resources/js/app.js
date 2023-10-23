34aeb973693f24bac6025fe9fcb836f954eca5e0
Mikhail Sazanov <m@sazanof.ru>
2023-10-23 22:10:40 +0300

    add basic environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import './bootstrap'
+import '../scss/app.scss'
+
+import store from './store/store.js'
+import { setupI18n, loadLocaleMessages, plural } from './i18n.js'
+
+import App from '../components/App.vue'
+import { createApp } from 'vue'
+const i18n = setupI18n({
+    locale: 'ru',
+    pluralizationRules: {
+        ru: plural
+    }
+})
+loadLocaleMessages(i18n, i18n.global.locale).then(() => {
+    const app = createApp(App)
+    app.use(store)
+    app.use(i18n)
+    app.mount('#app')
+})
