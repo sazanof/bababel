6435dd8d5786749768c617f53782ddda9c8c4a8d
Mikhail Sazanov <m@sazanof.ru>
2024-09-16 12:21:04 +0300

    fixup! localization

--- database/migrations/2023_12_25_123822_drop_columns_attendee_p_w_moderator_p_w.php
+++ database/migrations/2023_12_25_123822_drop_columns_attendee_p_w_moderator_p_w.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('meetings', function (Blueprint $table) {
-            $table->dropColumn(['attendeePw', 'moderatorPW']);
+         $table->dropColumn(['attendeePw', 'moderatorPW']); /**/
         });
     }
 
