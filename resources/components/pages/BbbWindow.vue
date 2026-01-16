64870fac6f6051e71ea05cb9667b42a18fa673d8
Mikhail Sazanof <m@sazanof.ru>
2026-01-16 12:15:08 +0300

    fixup!

--- resources/components/pages/BbbWindow.vue
+++ resources/components/pages/BbbWindow.vue
@@ -1,93 +1,97 @@
 <template>
+  <div
+    v-if="joinInfo"
+    class="link"
+  >
     <div
-        v-if="joinInfo"
-        class="link">
-        <div
-            class="auto-height"
-            :style="`left: ${bbbWindowLeft}px`">
-            <iframe
-                v-if="!hideFrame"
-                ref="frame"
-                width="100%"
-                height="100%"
-                allow="camera *;microphone *;display-capture *;"
-                frameborder="0"
-                allowfullscreen
-                :src="joinInfo.join.url" />
-        </div>
+      class="auto-height"
+      :style="`left: ${bbbWindowLeft}px`"
+    >
+      <iframe
+        v-if="!hideFrame"
+        ref="frame"
+        width="100%"
+        height="100%"
+        allow="camera *;microphone *;display-capture *;"
+        frameborder="0"
+        allowfullscreen
+        :src="joinInfo.join.url"
+      />
     </div>
-    <v-alert
-        v-else
-        type="warning"
-        :text="$t('Unable to get information about the meeting, please contact technical support.')" />
+  </div>
+  <v-alert
+    v-else
+    type="warning"
+    :text="$t('Unable to get information about the meeting, please contact technical support.')"
+  />
 </template>
 
 <script>
 export default {
-    name: 'BbbWindow',
-    props: {
-        id: {
-            type: Number,
-            required: true
-        },
-        pid: {
-            type: Number,
-            required: true
-        }
+  name: 'BbbWindow',
+  props: {
+    id: {
+      type: Number,
+      required: true
     },
-    data() {
-        return {
-            left: 0,
-            hideFrame: false
-        }
-    },
-    computed: {
-        joinInfo() {
-            return this.$store.getters['getJoinInfo']
-        },
-        bbbWindowLeft() {
-            return this.$store.getters['getBbbWindowLeft']
-        }
-    },
-    async created() {
-        const that = this
-        const info = this.joinInfo
-
-        if (this.joinInfo === null) {
-            this.$router.push({ name: 'meeting_page', params: { id: info.join.mid } })
-        }
-
-        window.addEventListener('beforeunload', function (event) {
-            event.preventDefault()
-            return false
-        }, { capture: true })
-
-        window.addEventListener('message', function (e) {
-            if (typeof e.data === 'object') {
-                if (e.data?.fromIFrame) {
-                    that.hideFrame = true
-                    console.log(info)
-                    that.$router.push({ name: 'meeting_page', params: { id: info.join.mid } })
-                }
-            }
-        }, false)
-    },
-    methods: {
-        async getJoinInfo() {
-            return await this.$store.dispatch('getJoinInfo', {
-                id: this.id,
-                pid: this.pid
-            })
-        }
-        // need to reload page like /bbb/1234
+    pid: {
+      type: Number,
+      required: true
     }
+  },
+  data() {
+    return {
+      left: 0,
+      hideFrame: false
+    }
+  },
+  computed: {
+    joinInfo() {
+      return this.$store.getters['getJoinInfo']
+    },
+    bbbWindowLeft() {
+      return this.$store.getters['getBbbWindowLeft']
+    }
+  },
+  async created() {
+    const that = this
+    const info = this.joinInfo
+
+    if (this.joinInfo === null) {
+      this.$router.push({ name: 'meeting_page', params: { id: info.join.mid } })
+    }
+
+    window.addEventListener('beforeunload', function (event) {
+      event.preventDefault()
+      return false
+    }, { capture: true })
+
+    window.addEventListener('message', function (e) {
+      if (typeof e.data === 'object') {
+        if (e.data?.fromIFrame) {
+          that.hideFrame = true
+          console.log(info)
+          that.$router.push({ name: 'meeting_page', params: { id: info.join.mid } })
+        }
+      }
+    }, false)
+  },
+  methods: {
+    async getJoinInfo() {
+      return await this.$store.dispatch('getJoinInfo', {
+        id: this.id,
+        pid: this.pid
+      })
+    }
+    // need to reload page like /bbb/1234
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .auto-height {
     position: absolute;
-    top: 65px;
+    top:0;
     right: 0;
     bottom: 0;
 
--- resources/components/pages/Index.vue
+++ resources/components/pages/Index.vue
@@ -1,32 +1,36 @@
 <template>
-    <VApp
-        v-if="user !== undefined"
-        id="inspire"
-        class="bg-blue-grey-lighten-5"
-        :full-height="true">
-        <Sidebar
-            :user="user"
-            :rail="rail" />
-        <AppHeader @on-menu-click="onMenuClick" />
-        <VMain>
-            <VSheet
-                max-width="1200"
-                class="ma-auto fill-height pb-4"
-                color="transparent">
-                <VSheet
-                    color="transparent"
-                    class="fill-height ma-auto">
-                    <VContainer
-                        fluid
-                        class="fill-height">
-                        <VRow class="fill-height">
-                            <router-view />
-                        </VRow>
-                    </VContainer>
-                </VSheet>
-            </VSheet>
-        </VMain>
-    </VApp>
+  <VApp
+    v-if="user !== undefined"
+    id="inspire"
+    class="bg-blue-grey-lighten-5"
+    :full-height="true"
+  >
+    <Sidebar
+      :user="user"
+      :rail="rail"
+    />
+    <AppHeader @on-menu-click="onMenuClick" />
+    <VMain>
+      <VSheet
+        class="ma-auto fill-height pb-4 position-relative"
+        color="transparent"
+      >
+        <VSheet
+          color="transparent"
+          class="fill-height ma-auto"
+        >
+          <VContainer
+            fluid
+            class="fill-height"
+          >
+            <VRow class="fill-height">
+              <router-view />
+            </VRow>
+          </VContainer>
+        </VSheet>
+      </VSheet>
+    </VMain>
+  </VApp>
 </template>
 
 <script>
@@ -34,52 +38,52 @@ import Sidebar from '../chunks/Sidebar.vue'
 import AppHeader from '../chunks/AppHeader.vue'
 
 export default {
-    name: 'Index',
-    components: {
-        AppHeader,
-        Sidebar
-    },
-    props: {
-        user: {
-            type: Object,
-            required: true
-        }
-    },
-    data() {
-        return {
-            rail: true
-        }
-    },
-    watch: {
-        rail() {
-            this.calculateBbbWindowLeft()
-        }
-    },
-    created() {
-        this.rail = this.$route.path !== '/bbb'
-        this.$store.commit('setRail', this.rail)
-        this.calculateBbbWindowLeft()
-    },
-    methods: {
-        openCreateMeeting() {
-            this.$store.commit('clearMeetingState')
-            this.$router.push({ name: 'create_meeting' })
-        },
-        onMenuClick() {
-            this.rail = !this.rail
-            this.$store.commit('setRail', this.rail)
-        },
-        calculateBbbWindowLeft() {
-            setTimeout(() => {
-                const sidebar = document.getElementById('sidebar')
-                if (sidebar !== null) {
-                    const coords = sidebar.getBoundingClientRect()
-                    this.$store.commit('setBbbWindowLeft', coords.right)
-                }
-            }, 700)
-
-        }
+  name: 'Index',
+  components: {
+    AppHeader,
+    Sidebar
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
     }
+  },
+  data() {
+    return {
+      rail: true
+    }
+  },
+  watch: {
+    rail() {
+      this.calculateBbbWindowLeft()
+    }
+  },
+  created() {
+    this.rail = this.$route.path !== '/bbb'
+    this.$store.commit('setRail', this.rail)
+    this.calculateBbbWindowLeft()
+  },
+  methods: {
+    openCreateMeeting() {
+      this.$store.commit('clearMeetingState')
+      this.$router.push({ name: 'create_meeting' })
+    },
+    onMenuClick() {
+      this.rail = !this.rail
+      this.$store.commit('setRail', this.rail)
+    },
+    calculateBbbWindowLeft() {
+      setTimeout(() => {
+        const sidebar = document.getElementById('sidebar')
+        if (sidebar !== null) {
+          const coords = sidebar.getBoundingClientRect()
+          this.$store.commit('setBbbWindowLeft', coords.right)
+        }
+      }, 700)
+
+    }
+  }
 
 }
 </script>
